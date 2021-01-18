6228d6909e6071638b542bf5ddba0a7bba8c9f0a
Vageswara Datta Maddala <mvdatta3@gmail.com>
2021-01-18 21:36:29 +0530

    Day-34 : Form palindrome with min. char. inserts

--- Day_34_FinishPalindromeWithMinChars.java
+++ Day_34_FinishPalindromeWithMinChars.java
@@ -10,6 +10,7 @@ There are seven other palindromes that can be made from "race" by adding three l
 but "ecarace" comes first alphabetically.
 
 As another example, given the string "google", you should return "elgoogle".
+
 */
 
 public class Day_34_FinishPalindromeWithMinChars{
@@ -22,26 +23,25 @@ public class Day_34_FinishPalindromeWithMinChars{
 		return helper(s.toCharArray(), 0, s.length()-1, new String[s.length()+1][s.length()+1]);
 	}
 
-
 	// Time O(n*n)
 	// Space O(n*n)
 	private static String helper(char[] s, int i, int j, String[][] dp){
-        if(i > j)
-            return "";
-        if(i == j)
-        	return s[i]+"";
+		if(i > j)
+			return "";
+		if(i == j)
+			return s[i]+"";
 
-        if(dp[i][j] != null)
-        	return dp[i][j];
+		if(dp[i][j] != null)
+			return dp[i][j];
 
-        if(s[i] == s[j])
-            return dp[i][j] = s[i]+helper(s, i+1, j-1,dp)+s[j];
-        
-        String left = s[i]+ helper(s, i+1, j,dp) +s[i], right = s[j]+ helper(s, i, j-1,dp) +s[j];
+		if(s[i] == s[j])
+			return dp[i][j] = s[i]+helper(s, i+1, j-1,dp)+s[j];
 
-        if(left.length() == right.length())
-        	return dp[i][j] = s[i] <= s[j] ? left : right;
+		String left = s[i]+ helper(s, i+1, j,dp) +s[i], right = s[j]+ helper(s, i, j-1,dp) +s[j];
 
-        return dp[i][j] = left.length() < right.length() ? left : right;
-    }
+		if(left.length() == right.length())
+			return dp[i][j] = s[i] <= s[j] ? left : right;
+
+		return dp[i][j] = left.length() < right.length() ? left : right;
+	}
 }
